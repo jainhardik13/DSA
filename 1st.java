f7ffb4fd0ccd4810f4e7c4d5c2d99f5cf8a194d1
jainhardik13 <jainhardik130@gmail.com>
2024-12-03 10:30:43 +0530

    appended

--- 1st.java
+++ 1st.java
@@ -1,5 +1,7 @@
-class Sample{
-    public static void main(String args[]){
+class Sample {
+    public static void main(String[] args) {
         System.out.println("Hello Developers...");
+        int number = 21;
+        System.out.println("The number is " + number);
     }
 }
